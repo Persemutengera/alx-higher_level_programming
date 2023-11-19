fff7e1d4b32419e5c36d6b081c428fbe50434bcf
Perseverance Mutengera <123206539+Persemutengera@users.noreply.github.com>
2023-11-19 15:35:25 +0200

    Update model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,33 +1,17 @@
 #!/usr/bin/python3
-"""
-Defines a state model that contain the class definition
- of a City and an instance Base = declarative_base()
-"""
-from lib2to3.pytree import Base
-from sre_parse import State
-from unicodedata import name
-from sqlalchemy import Column, ForeignKey, Integer, String, null
+""" Model State """
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+""" Use https://docs.sqlalchemy.org/en/13/orm/extensions/declarative/ """
 Base = declarative_base()
 
 
-class City(Base):
-    """
-    inherits from Base (imported from model_state)
-    links to the MySQL table cities
-
-    class attribute id that represents a column of
-    an auto-generated, unique integer, can't be null and is a primary key
-
-    class attribute name that represents a column
-    of a string of 128 characters and can't be null
-
-    class attribute state_id that represents a column
-    of an integer, can't be null and is a foreign key to states.id
-
-    """
-    __tablename__ = "cities"
-    id = Column(Integer, primary_key=True)
+class State(Base):
+    """ Class State inherith from Base
+    Implement model states """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True,
+                unique=True, nullable=False,
+                autoincrement=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
