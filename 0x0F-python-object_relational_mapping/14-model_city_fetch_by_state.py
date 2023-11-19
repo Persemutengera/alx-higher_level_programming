84d8e47a66ac928fb92384a44f11d759736ad37b
Perseverance Mutengera <123206539+Persemutengera@users.noreply.github.com>
2023-11-19 15:15:31 +0200

    Update 14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,22 +1,26 @@
-#!/usr/bin/python3
+!/usr/bin/python3
+""" Start link class to table in database
 """
-prints all City objects from the database hbtn_0e_14_usa
-"""
-
 import sys
-from venv import create
-from sqlalchemy import create_engine, true
-from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import Base, State
 from model_city import City
 
-if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    session_maker = sessionmaker(bind=engine)
-    session = session_maker()
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import asc
 
-    for city, state in session.query(City, State)\
-        .filter(City.state_id == State.id)\
-            .order_by(City.id):
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]),
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    citiesByState = session.query(City, State).join(
+        State).order_by(asc(City.id)).all()
+    for city, state in citiesByState:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
+    session.close()
