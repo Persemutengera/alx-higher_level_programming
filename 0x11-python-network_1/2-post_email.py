efc48e8ae867abe5cf60f7f03a8f755026d77b32
Perseverance Mutengera <123206539+Persemutengera@users.noreply.github.com>
2023-12-02 05:36:28 +0200

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-"""Sends a POST request to a given URL with a given email.
-Usage: ./2-post_email.py <URL> <email>
-  - Displays the body of the response.
-"""
-import sys
+"""Send a POST request to a url with the payload being an email address"""
+
 import urllib.parse
 import urllib.request
+import urllib.error
+import sys
 
-
-if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
-
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+try:
+    data = urllib.parse.urlencode({'email': sys.argv[2]}).encode()
+    with urllib.request.urlopen(sys.argv[1], data=data) as r:
+        print(r.read().decode('utf8'))
+except (urllib.error.URLError, IndexError) as e:
+    print(e)
